4887ec3a666176f9db8ea708d7a81e8d65e80fc5
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-10-11 16:14:41 -0700

    lots of progress - MVP met I think

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
@@ -35,13 +35,15 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      <Switch>
+        <Route path={`/movies/:id`}>
+          <Movie />
+        </Route>
 
-      <Route to={`/`}>
-        <MovieList movies={movieList}/>
-      </Route>
-      <Route to={`/movies/:id`}>
-        <Movie />
-      </Route>
+        <Route path={`/`}>
+          <MovieList movies={movieList}/>
+        </Route>
+    </Switch>
       {/* <div>Replace this Div with your Routes</div> */}
     </div>
   );
